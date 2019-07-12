101689d554a7c92770b0ab07e18f65b352a9c135
Jeison O. da Silva <jeison.desenvolvimento@gmail.com>
2019-07-12 00:46:38 -0300

    corrigido exportacao de dados (#9)

--- src/ParkingMob/app/src/main/java/com/jsonapp/parkingmob/ui/fragments/ParkingManangerFragment.java
+++ src/ParkingMob/app/src/main/java/com/jsonapp/parkingmob/ui/fragments/ParkingManangerFragment.java
@@ -118,8 +118,6 @@ public class ParkingManangerFragment extends Fragment {
      * >Communicating with Other Fragments</a> for more information.
      */
     public interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
-        void onFragmentInteraction(Uri uri);
         void requestCarData();
     }
 }
